722cdeeb0c2dd62230a40f3f4fdb4a42e67fa937
kevin <tu@email.com>
2026-02-27 12:06:52 -0500

    hola

--- Python/Hello_word.py
+++ Python/Hello_word.py
@@ -1,2 +1,8 @@
-print ('hola kevin')
-print ('hola soy una rama nueva que creaste en git')
+numero_1 = 100
+numero_2 = 200
+total = numero_1 * numero_2
+numero_3 = 400
+numero_4 =2000
+total_resta = numero_3 - numero_4
+print("El resultado de la multiplicación es:", total)
+print("El resultado de la resta es:", total_resta)
--- calculadpra.py
+++
@@ -1,5 +0,0 @@
-numero_1 = 2300
-numero_2 = 1000
-numero_3 = 2367
-total = numero_3 + numero_2 / (numero_1)
-print(total)
